6fb245728d62754545dbfe816c628ec97ac8e40f
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-05 14:11:59 +0600

    add query to find products that have never been ordered

---
+++ exists_operator.sql
@@ -0,0 +1,11 @@
+-- find the products that have never been ordered
+
+USE sql_store;
+
+SELECT * 
+FROM products p
+WHERE NOT EXISTS (
+	SELECT DISTINCT product_id
+	FROM order_items
+    WHERE product_id = p.product_id
+);
