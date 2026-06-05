5a12d280de1a5e82fb3f0da0394db699a740c2be
Abdullah Al Mridul <abdullah.github@zohomail.com>
2026-06-05 15:29:36 +0600

    fix: change JOIN to LEFT JOIN for invoices in sales query

--- subquery_in_select.sql
+++ subquery_in_select.sql
@@ -10,7 +10,7 @@ SELECT
     ) AS average,
     SUM(invoice_total) - (SELECT average) AS difference
 FROM clients c
-JOIN invoices 
+LEFT JOIN invoices 
 USING (client_id )
 GROUP BY (client_id)
 ORDER BY (client_id);
